44aba43875354960947eef199b888e80300afa92
ahmadhanialishaheen@gmail.com <ahmadhanialishaheen@gmail.com>
2023-08-06 12:44:22 +0300

    vierison-3

--- lib/api/local_data.dart
+++ lib/api/local_data.dart
@@ -1,15 +1,28 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:quran_kareem/models/ayah.dart';
 
 class LocalData {
-  Future<List<dynamic>> fetchData(int pageNumber) async {
+  Future<List<List<Ayah>>> fetchData(int pageNumber) async {
+    List<List<Ayah>> pageAyah = [];
     String result = await rootBundle.loadString('assets/hafs_smart_v8.json');
-
     if (result.isNotEmpty) {
       List<dynamic> ayate = jsonDecode(result);
-      List<dynamic> pageAyah =
-          ayate.where((element) => element['page'] == pageNumber).toList();
+
+      for (int i = 1; i <= 604; i++) {
+        List<Ayah> temp = [];
+
+        // ignore: avoid_function_literals_in_foreach_calls
+        ayate.forEach(
+          (element) {
+            if (element['page'] == pageNumber) {
+              temp.add(Ayah.fromJson(element));
+            }
+            return pageAyah.add(temp);
+          },
+        );
+      }
       return pageAyah;
     }
     return Future.error('error ');
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quran_kareem/api/local_data.dart';
+import 'package:quran_kareem/models/ayah.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +11,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   LocalData localData = LocalData();
-  int pageNumber = 2;
-  List<dynamic> ayate = [];
+  List<List<Ayah>>? pages;
+  int pageNumber = 3;
 
   void getLocalData() async {
-    ayate = await localData.fetchData(pageNumber);
+    pages = await localData.fetchData(pageNumber);
     setState(() {});
   }
 
@@ -40,82 +41,153 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: GestureDetector(
-        onTap: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Center(
-                  child: Text(
-                '$pageNumber',
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              )),
-              duration: const Duration(seconds: 1),
-            ),
-          );
-        },
-        child: SafeArea(child: PageView.builder(
-          itemBuilder: (context, index) {
-            pageNumber = index + 1;
-            getLocalData();
-            return Row(
-              textDirection:
-                  pageNumber % 2 == 0 ? TextDirection.ltr : TextDirection.rtl,
-              children: [
-                Expanded(
-                  child: Container(
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                        gradient: pageNumber % 2 == 0
-                            ? const LinearGradient(
-                                colors: [
-                                  Color(0xffffeec6),
-                                  Color(0xfffdfcfa),
-                                ],
-                              )
-                            : const LinearGradient(
-                                colors: [
-                                  Color(0xfffdfcfa),
-                                  Color(0xffffeec6),
-                                ],
-                              )),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Flexible(
-                            child: Wrap(
-                              children: [
-                                for (int i = 0; i < ayate.length; i++)
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      '${ayate[i]['aya_text']}',
-                                      style: const TextStyle(
+      body: pages == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : GestureDetector(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Center(
+                        child: Text(
+                      '$pageNumber',
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    )),
+                    duration: const Duration(seconds: 1),
+                  ),
+                );
+              },
+              child: SafeArea(
+                  child: PageView.builder(
+                itemCount: 604,
+                onPageChanged: (value) {
+                  setState(() {
+                    pageNumber = value;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  pageNumber = index + 1;
+                  getLocalData();
+                  Set<String?> surah = {};
+                  Set<int?> jozz = {};
+                  for (Ayah ayah in pages![index]) {
+                    surah.add(ayah.suraNameAr);
+                    jozz.add(ayah.jozz);
+                    return Row(
+                      textDirection: index % 2 == 0
+                          ? TextDirection.ltr
+                          : TextDirection.rtl,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: double.infinity,
+                            decoration: BoxDecoration(
+                                gradient: pageNumber % 2 == 0
+                                    ? const LinearGradient(
+                                        colors: [
+                                          Color(0xffffeec6),
+                                          Color(0xfffdfcfa),
+                                        ],
+                                      )
+                                    : const LinearGradient(
+                                        colors: [
+                                          Color(0xfffdfcfa),
+                                          Color(0xffffeec6),
+                                        ],
+                                      )),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Visibility(
+                                    visible: ayah.ayaNo == 1,
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 70,
+                                      decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                              'images/pngtree-head-of-surah.jpg'),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          '${ayah.suraNameAr}',
+                                          style: const TextStyle(
+                                            fontFamily: 'hafsSmart',
+                                            fontSize: 18,
+                                            textBaseline:
+                                                TextBaseline.ideographic,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Visibility(
+                                    visible: (ayah.ayaNo == 1 &&
+                                            ayah.suraNameAr != "الفَاتِحة" &&
+                                            ayah.suraNameAr != "التوبَة")
+                                        ? true
+                                        : false,
+                                    child: const Text(
+                                      '‏ ‏‏ ‏‏‏‏ ‏‏‏‏‏‏ ‏',
+                                      style: TextStyle(
                                         fontFamily: 'hafsSmart',
                                         fontSize: 24,
                                         textBaseline: TextBaseline.ideographic,
                                       ),
                                     ),
                                   ),
-                              ],
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Flexible(
+                                    child: Wrap(
+                                      children: [
+                                        for (Ayah ayah in pages![index])
+                                          Column(
+                                            children: [
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  '${ayah.ayaText}',
+                                                  style: const TextStyle(
+                                                    fontFamily: 'hafsSmart',
+                                                    fontSize: 18,
+                                                    textBaseline: TextBaseline
+                                                        .ideographic,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 5,
-                  color: Colors.red,
-                )
-              ],
-            );
-          },
-        )),
-      ),
+                        ),
+                        Container(
+                          width: 5,
+                          color: Colors.red,
+                        )
+                      ],
+                    );
+                  }
+                  return null;
+                },
+              )),
+            ),
     );
   }
 }
